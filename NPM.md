6cc26658063f028a9793e377c121553e973046f1
march <march.mao@verystar.cn>
2019-11-06 15:29:48 +0800

    params

---
+++ NPM.md
@@ -0,0 +1,27 @@
+# NPM
+
+
+## 简介
+
+NPM全称为Node Package Manager，是一个基于Node.js的包管理器，也是整个Node.js社区最流行、支持的第三方模块最多的包管理器。
+
+NPM的初衷：JavaScript开发人员更容易分享和重用代码。
+
+NPM的使用场景：
+
+* 允许用户获取第三方包并使用。
+* 允许用户将自己编写的包或命令行程序进行发布分享。
+
+NPM版本查询：npm -v 
+
+NPM安装：
+
+* 安装nodejs:由于新版的nodejs已经集成了NPM，所以可直接通过输入npm -v来测试是否成功安装。
+* 使用NPM命令来升级NPM:`npm install npm -g`
+
+网址:
+
+* 官网(https://www.npmjs.com/)
+* 官方文档(https://docs.npmjs.com/)
+* 中文文档(https://www.npmjs.cn/)
+* 初始NPM(https://www.cnblogs.com/john-sr/p/6036652.html)
--- params/main.go
+++ params/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
@@ -9,6 +11,7 @@ import (
 	"network"
 )
 
+// 测试网络请求参数获取情况
 var TestHandler network.HandlerFunc = func(c *network.Context) network.Response {
 	_ = c.Request.ParseForm()
 	_ = c.Request.ParseMultipartForm(32 << 20)
@@ -45,11 +48,68 @@ var TestHandler network.HandlerFunc = func(c *network.Context) network.Response 
 	})
 }
 
+type User struct {
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+	Page     int    `json:"page"`
+}
+
+// 测试GIN框架从请求中获取参数的方法
+// curl -d 'user_name=admin&password=123456' http://127.0.0.1:8080/params?user_name=zhangsan&page=1
+var ParamsHandler network.HandlerFunc = func(c *network.Context) network.Response {
+	// 查询请求URL后面的参数
+	userName1 := c.Query("user_name")
+	// 查询请求URL后面的参数,如果没有填写默认值
+	page := c.DefaultQuery("page", "0")
+	// 从表单中查询参数
+	userName2 := c.PostForm("user_name")
+
+	// POST和PUT主体参数优先于URL查询字符串值
+	userName3 := c.Request.FormValue("user_name")
+
+	// 返回POST并放置body参数,URL查询参数被忽略
+	userName4 := c.Request.PostFormValue("user_name")
+
+	// 从表单中查询参,如果没有填写默认值
+	password := c.DefaultPostForm("password", "111111")
+
+	var u1 User
+	// 从http.Request中读取值到User结构体中,手动确定绑定类型binding.Form
+	_ = c.BindWith(&u1, binding.Form)
+
+	var u2 User
+	// 从http.Request中读取值到User结构体中,根据请求方法类型和请求内容格式类型自动确定绑定类型
+	_ = c.Bind(&u2)
+
+	fmt.Println("userName1:", userName1)
+	fmt.Println("page:", page)
+	fmt.Println("userName2:", userName2)
+	fmt.Println("userName3:", userName3)
+	fmt.Println("userName4:", userName4)
+	fmt.Println("password:", password)
+	fmt.Printf("u1:%v\n", u1)
+	fmt.Printf("u2:%v\n", u2)
+
+	/*
+		userName1: zhangsan
+		page: 1
+		userName2: admin
+		userName3: admin
+		userName4: admin
+		password: 123456
+		u1:{  0}
+		u2:{  0}
+	*/
+
+	return c.JSON(nil)
+}
+
 func main() {
 	r := gin.Default()
 	r.Any("/test", network.Handle(TestHandler))
 	r.Any("/redirect", func(c *gin.Context) {
 		c.String(http.StatusOK, "redirect")
 	})
+	r.Any("/params", network.Handle(ParamsHandler))
 	_ = r.Run(":8080")
 }
